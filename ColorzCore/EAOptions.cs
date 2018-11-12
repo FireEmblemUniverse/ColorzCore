16d576695a25e0e638c31383d7a3dba467ee6d16
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-11-12 00:35:00 -0500

    Add framework for generalized compiler options. Add help, nowarn, noerr, werr, quiet.

---
+++ ColorzCore/EAOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorzCore
+{
+    class EAOptions
+    {
+        public bool werr;
+        public bool nowarn, nomess;
+
+        public EAOptions()
+        {
+            werr = false;
+            nowarn = false;
+            nomess = false;
+        }
+    }
+}
